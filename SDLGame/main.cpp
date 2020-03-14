ec41968555e68389639ac4e42d0e2171a13e0124
Ahmed Boulad <ahmedboulad95@gmail.com>
2020-03-14 14:10:39 -0500

    Initial Commit

---
+++ SDLGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDLGame
+//
+//  Created by Ahmed Boulad on 3/14/20.
+//  Copyright © 2020 ahmedboulad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
